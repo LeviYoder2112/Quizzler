9b8ee143db70c3270af51396eadff55ff6999c15
Levi <levi.yoder2112@gmail.com>
2018-11-06 11:33:41 -0700

    Created Question class and QuestionBank class

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+ 
+
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
@@ -21,12 +23,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+    questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
-    }
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        
+        }
+    
     
     
     func updateUI() {
@@ -40,7 +53,7 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
-        
+        if pickedAnswer == 
     }
     
     
